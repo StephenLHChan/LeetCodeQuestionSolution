be67badf9e820e9ef99dcb7b0429538a3bd0c44b
StephenLHChan <stephenc.1018@gmail.com>
2022-03-01 18:14:13 -0500

    Created

---
+++ src/com/stephenclhc/FirstBadVersion.java
@@ -0,0 +1,25 @@
+package com.stephenclhc;
+
+public class FirstBadVersion {
+    //278. First Bad Version
+
+
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+    public int firstBadVersion(int n) {
+
+        int left = 1;
+        int right = n;
+
+        int middle = 0;
+        while (left < right) {
+            middle = (left + right) / 2;
+
+            if (isBadVersion(middle))
+                right = middle;
+            else left = middle + 1;
+        }
+        return left;
+
+    }
+}
