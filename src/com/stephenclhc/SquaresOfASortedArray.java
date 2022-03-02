16acbc27bffb32a43d82f52b2c4d2a0e8384d4db
StephenLHChan <stephenc.1018@gmail.com>
2022-03-01 19:02:05 -0500

    Created

---
+++ src/com/stephenclhc/SquaresOfASortedArray.java
@@ -0,0 +1,24 @@
+package com.stephenclhc;
+
+public class SquaresOfASortedArray {
+    // 977. Squares of a Sorted Array
+
+    public int[] sortedSquares(int[] nums) {
+        var result = new int[nums.length];
+
+        int head = 0;
+        int tail = nums.length - 1;
+
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(Math.abs(nums[head]) > Math.abs(nums[tail])) {
+                result[i] = nums[head] * nums[head];
+                head++;
+            }
+            else{
+                result[i] = nums[tail] * nums[tail];
+                tail--;
+            }
+        }
+        return result;
+    }
+}
