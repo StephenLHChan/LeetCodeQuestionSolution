f772b5527a3e488d40f7d72953ee51be9776866d
StephenLHChan <stephenc.1018@gmail.com>
2022-03-01 23:43:24 -0500

    Created

---
+++ src/com/stephenclhc/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,15 @@
+package com.stephenclhc;
+
+public class BestTimeToBuyAndSellStock {
+    // 121. Best Time to Buy and Sell Stock
+    public int maxProfit(int[] prices) {
+        int min_price = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for(int price : prices){
+            min_price = Math.min(min_price, price);
+            profit = Math.max(profit, price - min_price);
+        }
+        return profit;
+    }
+}
