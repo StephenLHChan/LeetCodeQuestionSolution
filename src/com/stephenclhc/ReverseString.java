0477543f0b12d77a6c8703cc0e777f8ed0f1a230
StephenLHChan <stephenc.1018@gmail.com>
2022-03-01 23:51:53 -0500

    Created

---
+++ src/com/stephenclhc/ReverseString.java
@@ -0,0 +1,12 @@
+package com.stephenclhc;
+
+public class ReverseString {
+    // 344. Reverse String
+    public void reverseString(char[] s) {
+        for(int i = 0; i < s.length / 2; i++){
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+    }
+}
