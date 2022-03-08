3821ecf6b2bb6c7664a247252893525407f4094c
StephenLHChan <stephenc.1018@gmail.com>
2022-03-07 21:55:59 -0500

    created

---
+++ src/com/stephenclhc/ConvertSortedArrayToBST.java
@@ -0,0 +1,34 @@
+package com.stephenclhc;
+
+public class ConvertSortedArrayToBST {
+// 108. Convert Sorted Array to Binary Search Tree
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int left, int right){
+        if (left > right)
+            return null;
+
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBST(nums, left, mid - 1);
+        node.right = sortedArrayToBST(nums, mid + 1, right);
+
+        return node;
+    }
+
+}
