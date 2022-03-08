2f41bb7dc7c07954cfbfde510dd8a48b392e24d3
StephenLHChan <stephenc.1018@gmail.com>
2022-03-07 22:28:50 -0500

    created

---
+++ src/com/stephenclhc/RichestCustomerWealth.java
@@ -0,0 +1,16 @@
+package com.stephenclhc;
+
+public class RichestCustomerWealth {
+    // 1672. Richest Customer Wealth
+    public int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for (int[] account : accounts) {
+            int sum = 0;
+            for (int j = 0; j < accounts[0].length; j++)
+                sum += account[j];
+
+            max = Math.max(sum, max);
+        }
+         return max;
+    }
+}
