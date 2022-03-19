9633adc549a04472b7b4afd781dc968f5cfe1a88
StephenLHChan <stephenc.1018@gmail.com>
2022-03-19 19:58:25 -0400

    Q4 Python solution updated

--- Python/Median_of_Two_Sorted_Arrays.py
+++ Python/Median_of_Two_Sorted_Arrays.py
@@ -6,6 +6,6 @@ def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
     nums1.sort()
 
     if len(nums1) % 2 == 0:
-        return nums1[len(nums1) // 2] + nums1[len(nums1)//2 - 1] / 2
+        return (nums1[len(nums1) // 2] + nums1[len(nums1)//2 - 1]) / 2
     else:
         return nums1[len(nums1) // 2]
